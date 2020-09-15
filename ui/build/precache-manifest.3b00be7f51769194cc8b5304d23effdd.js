62917c2b95ff092d86aa3b06bbf879c5f871e513
Gianluca Righetto <gianluca@menttis.com>
2020-09-15 11:42:24 -0300

    New build for stargate-kiva into gh-pages.

---
+++ ui/build/precache-manifest.3b00be7f51769194cc8b5304d23effdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7fd43eb54f849d62cdbb6f6d4eec34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f1e115bcbb7b24857e",
+    "url": "/static/css/main.b177ca1b.chunk.css"
+  },
+  {
+    "revision": "a3c36c01a6dd48afff9f",
+    "url": "/static/js/2.47dcc099.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.47dcc099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f1e115bcbb7b24857e",
+    "url": "/static/js/main.325c59c8.chunk.js"
+  },
+  {
+    "revision": "5e38323b6587bf91e0d5",
+    "url": "/static/js/runtime-main.33f053cc.js"
+  }
+]);
